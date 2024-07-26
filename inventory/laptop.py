ebcb2a04be772e1e605862117aba95ac0a2d251a
likai0909 <23011764@myrp.edu.sg>
2024-07-26 10:33:53 +0800

    refactor (C) part 2

--- inventory/laptop.py
+++ inventory/laptop.py
@@ -29,30 +29,39 @@ class Laptop():
     def setIsAvailable(self, isAvailable):
         self._isAvailable = isAvailable
         
-    def loanLaptop(self, assetTag, dueDate):
+    def loanAsset(self, assetTag, dueDate):
         success = False
         if len(assetTag) > 0 and len(dueDate) > 0:
-            # Refactor (C): use findcamera()
-            for i in self.laptopList:
-                if i.getAssetTag() == assetTag:
-                    if i.getIsAvailable() == "Yes":
-                        i.setIsAvailable(False)
-                        i.setDueDate(dueDate)
-                        success = True
-        
+            foundAsset = self.findAsset(assetTag)
+            if foundAsset != None:
+                if foundAsset.getIsAvailable() == "Yes":
+                    foundAsset.setIsAvailable(False)
+                    foundAsset.setDueDate(dueDate)
+                    success = True
         return success
+            
+    def loanCamera(self, assetTag, dueDate):
+        return self.loanAsset(assetTag, dueDate)
+        
+    def loanLaptop(self, assetTag, dueDate):
+        return self.loanAsset(assetTag, dueDate)
     
+    def returnCamera(self, assetTag):
+        return self.returnAsset(assetTag)
+        
     def returnLaptop(self, assetTag):
+        return self.returnAsset(assetTag)
+    
+    def returnAsset(self, assetTag):
         success = False
         if len(assetTag) > 0:
-            # Refactor (C): use findcamera()
-            for i in self.laptopList:
-                if i.getAssetTag() == assetTag:
-                    if i.getIsAvailable() == "No":
-                        i.setIsAvailable(True)
-                        i.setDueDate("")
-                        success = True
-        
+            foundAsset = self.findAsset(assetTag)
+            if foundAsset != None:
+                if foundAsset.getIsAvailable() == "No":
+                    foundAsset.setIsAvailable(True)
+                    foundAsset.setDueDate("")
+                    success = True
         return success
+        
 
 
